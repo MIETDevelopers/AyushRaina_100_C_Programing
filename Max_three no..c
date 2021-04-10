dee736dded7517d968956dbfb88ff8f8cc457a53
AyushRaina007 <76679197+AyushRaina007@users.noreply.github.com>
2021-04-10 11:37:34 +0530

    Update Max_three no..c

--- Max_three no..c
+++ Max_three no..c
@@ -1,13 +1,17 @@
+// Purpose:a function which takes 3 numbers as input and prints maximum of those 3 numbers.
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     int x, y, z, result, max;
+	// if x is greater than both y and z, x is the largest
     printf("\nInput the first integer: "); 
     scanf("%d", &x);
+	// if y is greater than both x and z, y is the largest
     printf("\nInput the second integer: ");
     scanf("%d", &y);
+	// if z is greater than both y and x, z is the largest
     printf("\nInput the third integer: ");
     scanf("%d", &z);
     result=(x+y+abs(x-y))/2;
